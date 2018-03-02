a616b55d89bcba6b1c787bf040c29be6e1063efa
afrosuds <afrosuds@gmail.com>
2018-03-02 12:16:32 -0500

    Adding initial scenes.

---
+++ Assets/Game/Scripts/Environment UI/LoadEnvironment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadEnvironment : MonoBehaviour {
+
+    public GameObject loadingImage;
+
+    public void LoadScene(string sceneName)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(sceneName);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
