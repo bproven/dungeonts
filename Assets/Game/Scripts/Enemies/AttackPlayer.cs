def75fdcac750f8812239fffe7fd795d18bd9bc2
afrosuds <afrosuds@gmail.com>
2018-03-22 21:41:39 -0400

    Committing script changes for new agent modifications.

--- Assets/Game/Scripts/Enemies/AttackPlayer.cs
+++ Assets/Game/Scripts/Enemies/AttackPlayer.cs
@@ -6,7 +6,6 @@ public class AttackPlayer : MonoBehaviour {
 
     public GameObject player;
     public float moveSpeed;
-
 	// Use this for initialization
 	void Start () {
 		
@@ -14,15 +13,25 @@ public class AttackPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.GetComponent<Rigidbody2D>().velocity = (player.transform.position - gameObject.transform.position).normalized *moveSpeed * Time.deltaTime;
-	}
+
+        // Keep it simple
+        Vector2 newVelocity = (player.transform.position - gameObject.transform.position);
+        newVelocity.Normalize();
+        newVelocity.Scale(new Vector2(moveSpeed, moveSpeed));
+        gameObject.GetComponent<Rigidbody2D>().velocity = newVelocity;
+    }
 
     // Kill the player who we collided with
     void killPlayer(Collision2D coll)
     {
         Debug.Log("Enemy: Player killed.");
-        coll.gameObject.GetComponent<Agent>().done = true;
-        coll.gameObject.GetComponent<Agent>().reward -= 1;
+        // Punish all brains involved in this disaster
+        for (int i = 0; i < coll.transform.childCount; i++)
+            if (coll.transform.GetChild(i).GetComponent<Agent>())
+                coll.transform.GetChild(i).GetComponent<Agent>().done = true;
+
+        // Randomize my position so I can't spawn camp
+        transform.position = new Vector3(Random.Range(transform.parent.position.x - 2, transform.parent.position.x + 2), Random.Range(transform.parent.position.y - 2, transform.parent.position.y + 2), transform.parent.position.z);
     }
 
     void OnCollisionEnter2D (Collision2D coll)
