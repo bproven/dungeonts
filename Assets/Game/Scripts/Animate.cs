4fd025cc57a0ba78b17a75c628b0ed010f967d8d
Cole Degeorges <coledegeorges@gmail.com>
2018-03-27 19:05:55 -0400

    Added code for animation

---
+++ Assets/Game/Scripts/Animate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+
+    private Vector2 lastPos;
+    private SpriteRenderer mySprite;
+    Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        lastPos = gameObject.transform.position;
+
+        mySprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Vector2 curPos = gameObject.transform.position;
+        if (curPos == lastPos) {
+            anim.SetTrigger("idle");
+        }
+        else
+        {
+            anim.SetTrigger("run");
+            Vector2 direction = (curPos - lastPos);
+            if (direction.x < 0)
+                mySprite.flipX = true;
+            else
+                mySprite.flipX = false;
+        }
+        lastPos = curPos;
+    }
+}
