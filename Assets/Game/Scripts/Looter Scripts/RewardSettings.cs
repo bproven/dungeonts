6d0f531c1f48b25598b919226adfd7f8f39cd2cb
JeremyLuna27 <jeremyluna27@yahoo.com>
2018-04-13 09:23:17 -0400

    rewards menu added

--- Assets/Game/Scripts/Looter Scripts/RewardSettings.cs
+++ Assets/Game/Scripts/Looter Scripts/RewardSettings.cs
@@ -15,4 +15,27 @@ public class RewardSettings : MonoBehaviour {
         collide_wall;
 
     public static Dictionary<string, int> item_values = new Dictionary<string, int>();
+
+
+    public void AdjustProxGold(float newProx_gold)
+    {
+        prox_gold = newProx_gold;
+    }
+
+    public void AdjustAttackEnemy(float newAttack_enemy)
+    {
+        attack_enemy = newAttack_enemy;
+    }
+
+    public void AdjustTakeDamage(float newTake_damage)
+    {
+        take_damage = newTake_damage;
+    }
+
+    public void AdjustProxWall(float newProx_wall)
+    {
+        prox_wall = newProx_wall;
+    }
+
+
 }
